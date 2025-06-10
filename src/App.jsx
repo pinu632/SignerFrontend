f927fe3abec6c20a79ef0766f715e6edf3aa0084
pinu632 <pinu632@gmail.com>
2025-06-10 22:38:51 +0530

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import './App.css'
 
 import  Canvas  from './Components/Canvas'
-import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import {Routes,Route} from 'react-router-dom'
 import Dashboard from './Pages/Dashboard'
 import DocumentUpload from './Pages/Upload'
 
@@ -12,26 +12,23 @@ function App() {
 
 
   return (
-    <>
-    
-{/* 
-      <Canvas/> */}
-      {/* <SignaturePad/> */}
+  
 
 
-      <BrowserRouter>
+
+     
            <Routes>
                   <Route path='/' element={<Dashboard/>}/>
                   <Route path='/upload' element={<DocumentUpload/>} />
                   <Route path='/:action/:documentId/:email' element={<Canvas/>} />
                   
            </Routes>
-      </BrowserRouter>
+    
       
  
     
       
-    </>
+
   )
 }
 
